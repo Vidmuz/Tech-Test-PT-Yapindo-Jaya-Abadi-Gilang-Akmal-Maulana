b5c93c9ffc165a52e83f38f4d7f9f3e7eef135fc
Vidmuz <itemlu26@gmail.com>
2026-07-23 10:38:52 +0700

    mengkonfigurasi nextjs Tailwind dan ESLint

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "github.com",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
+    ],
+  },
+  experimental: {
+    optimizePackageImports: ["lucide-react", "framer-motion"],
+  },
+};
+
+export default nextConfig;
